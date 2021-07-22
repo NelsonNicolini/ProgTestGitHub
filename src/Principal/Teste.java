3e1a5c1703c0ec5015c4aadeeac8ca4370dbfbf3
NelsonNicolini <nelsonicolin@yahoo.com>
2021-07-22 17:50:15 -0300

    Animacao

--- src/Principal/Teste.java
+++ src/Principal/Teste.java
@@ -2,10 +2,34 @@ package Principal;
 
 public class Teste {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 	
 		System.out.println("Olá, GitHub!");
+		
+		System.out.println();
+		for (int i=0; i<10; i++) {			
+			
+			System.out.print("_");
+			delay();
+			System.out.print("\\");
+			delay();
+			System.out.print("|");
+			delay();
+			System.out.print("/");	
+			delay();
+						
+		}
+	}
 
+	private static void delay() {
+		int w=5;
+		for (int j=0; j<300000000; j++) {
+			for (int k=0; k<300000000; k++) {
+				w=-w;				
+			}
+			
+		}
+		
 	}
 
 }
